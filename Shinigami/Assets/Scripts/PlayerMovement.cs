10051fcb365ee79f1dc910d4820c1732a54e02de
Maria Isabel Gomez <mariai.gomezr@gmail.com>
2022-02-07 19:06:00 -0500

    Correccion de dash y Adicion de Animaciones

--- Shinigami/Assets/Scripts/PlayerMovement.cs
+++ Shinigami/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
+
     [SerializeField] float runSpeed = 20f;
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
     bool dash = false;
+    float timer = 0f;
     void Update()
     {
         horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+        
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
         if (Input.GetButtonDown("Crouch"))
         {
@@ -24,19 +31,38 @@ public class PlayerMovement : MonoBehaviour
         {
             crouch = false;
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && !dash)
         {
             dash = true;
-        }else if (Input.GetKeyUp(KeyCode.E))
+            
+        }
+        else if (dash && controller.dashTime<=0)
         {
             dash = false;
+            controller.dashTime = controller.startDashTime;
+
+
         }
+        /*else if (Input.GetKeyUp(KeyCode.E))
+        {
+            dash = false;
+
+        }*/
+    }
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnRolling(bool isRolling)
+    {
+        animator.SetBool("IsRolling", isRolling);
     }
     private void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump, dash);
         jump = false;
-        dash = false;
+        //dash = false;
 
     }
 
