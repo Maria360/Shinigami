199a1c92319e979ed7386475cdde5ceb9507a272
Maria Isabel Gomez <mariai.gomezr@gmail.com>
2022-02-27 16:25:01 -0500

    Dummy HealthBar

--- Shinigami/Assets/Scripts/Enemy/Dummy.cs
+++ Shinigami/Assets/Scripts/Enemy/Dummy.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dummy : MonoBehaviour
 {
     public float dummyHealth;
     public float damage2Dummy;
     public ParticleSystem deadDummyEffect;
-
+    public GameObject HealthBarPanel;
+    Slider slider;
+    private void Start()
+    {
+        HealthBarPanel.SetActive(false);
+        slider = HealthBarPanel.GetComponent<Slider>();
+        slider.value = dummyHealth;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Wepon"))
         {
+            HealthBarPanel.SetActive(true);
             dummyHealth -= damage2Dummy;
+            slider.value = dummyHealth;
+
             if (dummyHealth<= 0)
             {
                 deadDummyEffect.Play();
-
                 Destroy(gameObject);
             }
         }
