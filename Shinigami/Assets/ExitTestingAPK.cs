2b7644725bac8e9cb35f9f9e41b100d9f3d10409
Maria Isabel Gomez <mariai.gomezr@gmail.com>
2022-03-09 14:55:37 -0500

    Homescreen
    Funciona el boton de exit y el de start inicia el tutorial
    La idea es hacer un solo script que maneje todo en ese menu, settings, exit, start y levels

--- Shinigami/Assets/ExitTestingAPK.cs
+++ Shinigami/Assets/ExitTestingAPK.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitTestingAPK : MonoBehaviour
 {
@@ -8,5 +9,9 @@ public class ExitTestingAPK : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("EXIT");
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
