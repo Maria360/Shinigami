4406318685eaa4643076685ff4a27bd762c66bc1
Maria Isabel Gomez <mariai.gomezr@gmail.com>
2022-03-08 23:47:27 -0500

    Pausa funcional
    El juego se pausa y se resume
    FALTA que cargue el homescreen y que reinicie el juego

--- Shinigami/Assets/Scripts/PauseMenu.cs
+++ Shinigami/Assets/Scripts/PauseMenu.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
     public void Pause()
     {
-
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
     public void Resume()
     {
-
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
     }
 }
